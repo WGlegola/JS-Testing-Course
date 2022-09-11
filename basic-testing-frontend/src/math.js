de2dd70e7e0b7b74fd3b643d0f4964ea0ba41f88
Wojciech Glegola <wglegola@gmail.com>
2022-09-11 22:24:44 +0200

    added integration tests

--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -1,3 +1,5 @@
+import { cleanNumbers } from "./util/numbers";
+
 export function add(numbers) {
   let sum = 0;
 
@@ -5,4 +7,14 @@ export function add(numbers) {
     sum += +number;
   }
   return sum;
+}
+
+export function calculateResult(numberValues) {
+  try {
+    const numbers = cleanNumbers(numberValues);
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+  return result;
 }
